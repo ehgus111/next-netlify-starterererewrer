2ad97fed34a9c5e07eee807582d0100834b8ee25
ehgus111 <115925853+ehgus111@users.noreply.github.com>
2022-11-05 23:00:06 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>도현!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to the 사장님의 site" />
+        <Header title="도현이의 사이트에 오신것을 환영합니" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        
         </p>
       </main>
 
